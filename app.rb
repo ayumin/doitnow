6fa601208efd66c0c3076c76a3bf30eb0fee0931
omega <metallic.code@gmail.com>
2013-03-17 22:54:36 +0900

    cairo_test

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ get '/doitnow' do
   context.set_source_rgb(25, 255, 255)
   context.font_size = 25
   context.move_to(10, 50)
-  context.show_text(params[:url])
+  context.show_text(params[:string])
   #Drawing background-color(Black)
   surface.write_to_png('views/paint.png')
   #Sent to doitnow.haml 
@@ -42,15 +42,15 @@ get '/doitnow' do
 
 
 helpers do
-  def add_schema(url)
-    if url !~ /^https?/
-      if url =~ /\/\//
-        "http:#{url}"
+  def add_schema(string)
+    if string !~ /^https?/
+      if string =~ /\/\//
+        "http:#{string}"
       else
-        "http://#{url}"
+        "http://#{string}"
       end
     else
-      url
+      string
     end
   end
 
